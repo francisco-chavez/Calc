c8f4798e19b506c65a5fd6e1aacf9c0374267213
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-12-29 18:15:43 -0800

    Starting work on component that will do actual calculation.

---
+++ src/CalcSLN/CalcWPF/CalcInput.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace Unv.CalcWPF
+{
+	public enum CalcInput
+	{
+		NumKey0 = 0,
+		NumKey1,
+		NumKey2,
+		NumKey3,
+		NumKey4,
+		NumKey5,
+		NumKey6,
+		NumKey7,
+		NumKey8,
+		NumKey9,
+		KeyPoint,
+		KeyInvertSign,
+		KeyPercent,
+		KeySquareRoot,
+		KeyEquals,
+		KeyClear,
+		KeyAdd,
+		KeySubtract,
+		KeyMultiply,
+		KeyDivide,
+		KeyMemoryClear,
+		KeyMemoryRetrieve,
+		KeyMemoryAdd,
+		KeyMemorySubtract
+	}
+}
---
+++ src/CalcSLN/CalcWPF/TheBrain.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Unv.CalcWPF
+{
+	public class TheBrain
+		: INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private double _reg00;
+		private double _reg01;
+		private double _regM;
+
+
+		public TheBrain()
+		{
+			_reg00	= 0d;
+			_reg01	= 0d;
+			_regM	= 0d;
+		}
+
+
+		public void NewInput(CalcInput input)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
