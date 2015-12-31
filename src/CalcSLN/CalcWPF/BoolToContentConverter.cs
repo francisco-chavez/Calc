7fd499cf4f0321c5095bdb12f90766e460faf923
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2015-12-31 12:47:20 -0800

    Added BoolToContentConverter class.
    This class might be a bit of overkill for this project, but I created it
    because it's really great at what it does. It cuts down on the number of
    BoolToXXXXConverter classes by replacing them all.

---
+++ src/CalcSLN/CalcWPF/BoolToContentConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+
+namespace Unv.CalcWPF
+{
+	public class BoolToContentConverter
+		: IValueConverter
+	{
+		public object TrueContent	{ get; set; }
+		public object FalseContent	{ get; set; }
+		public object NullContent	{ get; set; }
+
+		public BoolToContentConverter()
+		{
+			TrueContent		= "True";
+			FalseContent	= "False";
+			NullContent		= "Null";
+		}
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			bool? boolValue = value as bool?;
+			return boolValue.HasValue ? (boolValue.Value ? TrueContent : FalseContent) : NullContent;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
